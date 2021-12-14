5ca16b1d7926e8b9e52e183bdc08c135adcbc70d
Mantecorso <luisjuradoquesada@gmail.com>
2021-12-14 09:38:17 +0100

    lluvia colores

--- main.js
+++ main.js
@@ -7,5 +7,31 @@ window.addEventListener('load', function() {
             this.classList.add('active');
         });
     }
-    list.forEach((item) => item.addEventListener('click', activar)) ;
+    list.forEach((item) => item.addEventListener('click', activar));
+
+    function rain() {
+        let uno = 111;
+        let goldo = document.querySelector('.contenedor');
+        let i = 0;
+
+        while (i < uno) {
+            let gotas = document.createElement('i');
+            let stamano = Math.random() * 5;
+            let posicionX = Math.floor(Math.random() * window.innerWidth);
+            let retraso = Math.random() * -20;
+            let duracion = Math.random() * 5;
+
+            gotas.style.width = 0.2 + stamano + "px";
+            gotas.style.left = posicionX + "px";
+            gotas.style.animationDelay = retraso + "s";
+            gotas.style.animationDuration = 1 + duracion + "s";
+
+            goldo.appendChild(gotas);
+            i++;
+        }
+
+    }
+
+    rain();
+
 })
